49d13680e00e59e8c7b29c92016f622149f18947
edom18 <edo.m18@gmail.com>
2021-10-13 22:03:43 +0900

    feat: Added GUI buttons.

--- Assets/Project/Scripts/StampController.cs
+++ Assets/Project/Scripts/StampController.cs
@@ -1,19 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StampController : MonoBehaviour
 {
     [SerializeField] private Stamper _stamper = null;
+    [SerializeField] private Texture2D[] _textures;
     [SerializeField] private float _height = 0.4f;
-    
+
+    private int _index = 0;
+
+    private void Start()
+    {
+        _stamper.StampTexture = _textures[0];
+    }
+
     private void Update()
     {
-        UpdateStampPosition();
-
         if (Input.GetMouseButtonDown(0))
         {
+            UpdateStampPosition();
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 130, 30), "足跡"))
+        {
             _stamper.Stamp();
+        }
+
+        if (GUI.Button(new Rect(150, 10, 130, 30), "変更"))
+        {
+            Change();
         }
     }
 
@@ -21,7 +41,7 @@ public class StampController : MonoBehaviour
     {
         Vector3 pos = Input.mousePosition;
         pos.z = Camera.main.nearClipPlane;
-        
+
         Ray ray = Camera.main.ScreenPointToRay(pos);
         if (!Physics.Raycast(ray, out RaycastHit hit))
         {
@@ -35,5 +55,11 @@ public class StampController : MonoBehaviour
         Vector3 up = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up);
         Vector3 forward = Vector3.Cross(Camera.main.transform.right, up);
         _stamper.transform.rotation = Quaternion.LookRotation(forward, up);
+    }
+
+    private void Change()
+    {
+        _index = (_index + 1) % _textures.Length;
+        _stamper.StampTexture = _textures[_index];
     }
 }
